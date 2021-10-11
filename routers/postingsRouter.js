1ab3b0f8f6e11ddbb2ae9cebbb563bdb8ce80b7e
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-10-11 17:23:57 +0900

    Postings CRUD

--- routers/postingsRouter.js
+++ routers/postingsRouter.js
@@ -1,11 +1,21 @@
 import express from "express";
-
+import {
+  getAllPostings,
+  postPostings,
+  getOnePosting,
+  patchPosting,
+  deletePosting,
+} from "../controller/postings.js";
 // import { authMiddleware } from '../middlewares/authMiddleware.js';
 const postingRouter = express.Router();
 
-postingRouter.route("/").get();
+postingRouter.route("/").get(getAllPostings).post(postPostings);
 
-postingRouter.route("/:id");
+postingRouter
+  .route("/:id")
+  .get(getOnePosting)
+  .delete(deletePosting)
+  .patch(patchPosting);
 
 postingRouter.route("/:id/comments");
 
