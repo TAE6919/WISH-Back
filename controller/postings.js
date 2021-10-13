f6ab263e95626fcbfcaa5242f1c27f71d05b3a68
KimGiTae <rlxo6919@naver.com>
2021-10-13 16:43:56 +0900

    수정해주세요

--- controller/postings.js
+++ controller/postings.js
@@ -2,31 +2,19 @@ import { Content, Like } from '../models/postings.js';
 import mongoose from 'mongoose';
 import { nowDate } from '../library/time.js';
 
-//테스트용
-<<<<<<< HEAD
-const userId = mongoose.Types.ObjectId('c1f93bc87aa222bd5bb7a4eb');
-const temp = mongoose.Types.ObjectId('c2f93bc87aa222bd5bb7a4e2');
-const nick = 'John Doe';
-=======
-// const userId = mongoose.Types.ObjectId("c1f93bc87aa222bd5bb7a4eb");
-// const temp = mongoose.Types.ObjectId("c2f93bc87aa222bd5bb7a4e2");
-// const nick = "John Doe";
->>>>>>> da210a71af81cba5c76ebcf550a9126040fd5378
-// const postingId = mongoose.Types.ObjectId("6163f627fd74d1cbe34d8f0b");
-
 // 게시물 생성(CREATE)
 export const postPostings = async (req, res) => {
-  const { title, text, imageUrl } = req.body;
-  // const { file } = req;
-  // const imageUrl = file.path;
-  const { _id, nick } = req.user;
-
+  const { title, text } = req.body;
+  const { file } = req;
+  const imageUrl = file.path;
+  // const { userId } = req.user;
+  console.log(req.body);
   try {
     // 사용자 조회 - nick을 가져오기 위해 필요
     // const user = await User.findById(userId);
 
     const posting = {
-      authorID: _id,
+      authorID: userId,
       authorName: nick,
       imageUrl,
       title,
@@ -55,8 +43,8 @@ export const getAllPostings = async (req, res) => {
 
 //특정 게시물 조회 (READ ONE)
 export const getOnePosting = async (req, res) => {
-  console.log(req.user);
   const { postingId } = req.params;
+
   try {
     const posting = await Content.findById(postingId);
     return res.status(200).json(posting);
@@ -69,19 +57,16 @@ export const getOnePosting = async (req, res) => {
 // 특정 게시물의 일부 속성 수정
 export const patchPosting = async (req, res) => {
   const { postingId } = req.params;
+  console.log(req.body);
+  console.log(req.params);
   const { imageUrl, title, text } = req.body;
-  const { _id } = req.user;
+  // const { userId } = req.user;
 
   try {
     const posting = await Content.findById(postingId);
     // 토큰 id랑 해당 게시물의 작성자 id 비교
-<<<<<<< HEAD
     if (!posting.authorID.equals(userId)) {
       console.log('사용자 일치하지 않음');
-=======
-    if (!posting.authorID.equals(_id)) {
-      console.log("사용자 일치하지 않음");
->>>>>>> da210a71af81cba5c76ebcf550a9126040fd5378
       return res.sendStatus(400);
     }
 
@@ -104,12 +89,12 @@ export const patchPosting = async (req, res) => {
 // 특정 게시물을 삭제
 export const deletePosting = async (req, res) => {
   const { postingId } = req.params;
-  const { _id } = req.user;
+  // const userId = req.user;
 
   try {
     const posting = await Content.findById(postingId);
 
-    if (!posting.authorID.equals(_id)) return res.sendStatus(400);
+    if (posting.authorID.equals(userId)) return res.sendStatus(400);
     // const posting = await Content.findByIdAndDelete(postingId);
     await Content.deleteOne(posting);
     return res.sendStatus(200);
@@ -122,7 +107,7 @@ export const deletePosting = async (req, res) => {
 //좋아요
 export const postLike = async (req, res) => {
   const { postingId } = req.params;
-  const { _id } = req.user;
+  // const { userId } = req.user;
 
   try {
     // 해당 사용자가 좋아요를 눌렀는지 확인
@@ -143,13 +128,13 @@ export const postLike = async (req, res) => {
     if (result.length === 0) {
       await Content.updateOne(posting, {
         $push: {
-          Like: { likedUser: _id },
+          Like: { likedUser: userId },
         },
       });
     } else {
       await Content.updateOne(posting, {
         $pull: {
-          Like: { likedUser: _id },
+          Like: { likedUser: userId },
         },
       });
     }
