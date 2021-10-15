90a8467b720e6337b062ea119311e19084f2d18c
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-10-15 14:10:38 +0900

    수정

--- middlewares/signupMiddleware.js
+++ middlewares/signupMiddleware.js
@@ -1,14 +1,17 @@
-import signUpSchema from "../validations/signup.js"
+import signUpSchema from "../validations/signup.js";
 
 export const validateSignUp = async (req, res, next) => {
-  const { nick, email, password, confirmPassword } = req.body
+  const { nick, email, password, confirmPassword } = req.body;
 
   try {
     //회원가입 절차일때만 서로 포함하는지 확인
     if (nick && confirmPassword) {
       // nick, password 서로 포함 여부
-      const isIncluded = nick.includes(password) || password.includes(nick)
-      if (isIncluded) return res.status(400).send({ msg: "닉네임 과 패스워드에 중복이 있습니다." })
+      const isIncluded = nick.includes(password) || password.includes(nick);
+      if (isIncluded)
+        return res
+          .status(400)
+          .send({ msg: "닉네임 과 패스워드에 중복이 있습니다." });
     }
 
     const value = await signUpSchema.validateAsync({
@@ -16,11 +19,11 @@ export const validateSignUp = async (req, res, next) => {
       email,
       password,
       confirmPassword,
-    })
-    console.log(value)
-    next()
+    });
+    console.log(value);
+    next();
   } catch (error) {
-    console.log(error)
-    return res.status(400).send({ msg: "양식을 확인하세요." })
+    console.log(error);
+    return res.status(400).send({ message: "양식을 확인하세요." });
   }
-}
+};
